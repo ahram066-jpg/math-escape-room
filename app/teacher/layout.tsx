4a66585cfb80cbce9f86a278921a4096b8f16e6f
ahram066-jpg <ahram066@gmail.com>
2026-09-07 10:07:31 +0900

    교사용 레이아웃에 실시간 참여 현황 연결

--- app/teacher/layout.tsx
+++ app/teacher/layout.tsx
@@ -1,10 +1,12 @@
 import type { ReactNode } from "react";
+import TeacherActivitySummary from "./TeacherActivitySummary";
 import TeacherLeaderboard from "./TeacherLeaderboard";
 
 export default function TeacherLayout({ children }: { children: ReactNode }) {
   return (
     <>
       {children}
+      <TeacherActivitySummary />
       <TeacherLeaderboard />
     </>
   );
